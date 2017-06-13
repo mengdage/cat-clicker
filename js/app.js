e53a8808be9cb75c2cd8e869b21f6a711a96edcf
meng <wulalameng@gmail.com>
2017-06-13 19:33:46 -0400

    add admin panel

--- js/app.js
+++ js/app.js
@@ -1,42 +1,96 @@
 (function(){
   'use strict';
   // model
-  var CatMaker = function(id, name, url) {
-    this.id = id;
-    this.name = name;
-    this.url = url;
-    this.number = 0;
+  var model = {
+    currentCat: null,
+    cats: [
+      {
+        id: 0,
+        name: 'cat_1',
+        url: 'images/cat_1.jpg',
+        number: 0
+      },
+      {
+        id: 1,
+        name: 'cat_2',
+        url: 'images/cat_2.jpg',
+        number: 0
+      },
+      {
+        id: 2,
+        name: 'cat_3',
+        url: 'images/cat_3.jpg',
+        number: 0
+      },
+      {
+        id: 3,
+        name: 'cat_4',
+        url: 'images/cat_4.jpg',
+        number: 0
+      },
+      {
+        id: 4,
+        name: 'cat_5',
+        url: 'images/cat_5.jpg',
+        number: 0
+      },
+      {
+        id: 5,
+        name: 'cat_6',
+        url: 'images/cat_6.jpg',
+        number: 0
+      }
+    ],
+    // change current cat to other cat
+    changeCurrentCat: function(id) {
+      this.currentCat = this.cats[id];
+    },
+
+    // update current cat's statistics
+    updateCurrentCat: function(newData) {
+      this.currentCat.name = newData.name;
+      this.currentCat.url = newData.url;
+      this.currentCat.number = newData.number;
+    }
   };
 
-  var cats = [];
-  cats.push(new CatMaker(0, 'cat_1', 'images/cat_1.jpg'));
-  cats.push(new CatMaker(1, 'cat_2', 'images/cat_2.jpg'));
-  cats.push(new CatMaker(2, 'cat_3', 'images/cat_3.jpg'));
-  cats.push(new CatMaker(3, 'cat_4', 'images/cat_4.jpg'));
-  cats.push(new CatMaker(4, 'cat_5', 'images/cat_5.jpg'));
-  cats.push(new CatMaker(5, 'cat_6', 'images/cat_6.jpg'));
 
   // octopus
   var octopus = {
+    adminToggle: false,
+
+    toggleAdmin: function() {
+      this.adminToggle = !this.adminToggle;
+      view.renderAdminView();
+    },
+
     init: function() {
-      this.currentCat = cats[0] || null;
+      model.changeCurrentCat(0);
       view.init();
     },
 
     changeCat: function(catId) {
-      if(this.currentCat.id != catId) {
-        this.currentCat = cats[catId];
+      if(model.currentCat.id != catId) {
+        model.changeCurrentCat(catId);
         view.renderContainer();
       }
     },
 
     getCurrentCat: function() {
-      return this.currentCat;
+      return model.currentCat;
     },
 
-    increaseClickNumber: function() {
-      this.currentCat.number++;
+    getCats: function() {
+      return model.cats;
+    },
+
+    increaseCurClickNumber: function() {
+      model.currentCat.number++;
       view.renderClickNumber();
+    },
+    updateCurCat: function(newData) {
+      model.updateCurrentCat(newData);
+      view.renderContainer();
     }
   };
 
@@ -46,12 +100,13 @@
     init : function() {
       this.initCatsList();
       this.initCatContainer();
+      this.initAdminView();
     },
 
     initCatsList: function() {
       this.catItemTemplate = $('script[data-template="catItem"]').html();
       this.$catsList = $('.catsList');
-      console.log(this.$catsList);
+      var cats = octopus.getCats();
       for(var i = 0; i < cats.length; i++) {
         var catItem = this.catItemTemplate;
         catItem = catItem.replace(/{{id}}/g, i).replace(/{{url}}/g, cats[i].url).replace(/{{name}}/g, cats[i].name);
@@ -69,7 +124,7 @@
     initCatContainer : function() {
       this.$catContainer = $('.catContainer');
       this.$catContainer.on('click', '.catImg', function(e) {
-        octopus.increaseClickNumber();
+        octopus.increaseCurClickNumber();
       });
       this.renderContainer();
     },
@@ -77,7 +132,6 @@
     renderContainer: function() {
       var curCat = octopus.getCurrentCat();
       var $catContainer = this.$catContainer;
-      $catContainer.attr('data-id', curCat.id);
       $catContainer.find('.catName').text(curCat.name);
       $catContainer.find('.catImg').attr('src', curCat.url);
       // $catContainer.find('.clickNumber').text(curCat.number);
@@ -88,6 +142,59 @@
       var curCat = octopus.getCurrentCat();
       var $catContainer = this.$catContainer;
       $catContainer.find('.clickNumber').text(curCat.number);
+    },
+
+    initAdminView: function() {
+      var curCat = octopus.getCurrentCat();
+      this.$adminView = $('.adminView');
+      var $adminView = this.$adminView;
+      this.$adminInfo = $adminView.find('.admin-info');
+      var $adminInfo = this.$adminInfo;
+      var $adminBtn = $adminView.find('.admin-btn');
+
+      // add event listern for the admin button
+      $adminBtn.on('click', function(e) {
+        octopus.toggleAdmin();
+      });
+
+      // add event listener for the cancel button
+      $adminInfo.find('.cancel-btn').on('click', function(e) {
+        // tell octopus to close admin view
+        octopus.toggleAdmin();
+      });
+      // add event listener for the save button
+      $adminInfo.find('.save-btn').on('click', function(e) {
+        // create new data for updating the current cat
+        var newCat = {
+          name: $adminInfo.find('#admin-name').val(),
+          url: $adminInfo.find('#admin-url').val(),
+          number: $adminInfo.find('#admin-clicks').val()
+        };
+        // tell octopus to update the current cat
+        octopus.updateCurCat(newCat);
+        // tell octopus to close admin view
+        octopus.toggleAdmin();
+      });
+
+      this.renderAdminView();
+
+
+    },
+
+    renderAdminView: function() {
+      var adminStatus = octopus.adminToggle;
+      var $adminInfo = this.$adminInfo;
+      var curCat = octopus.getCurrentCat();
+      if(adminStatus){ // open admin view
+        $adminInfo.find('#admin-name').val(curCat.name);
+        $adminInfo.find('#admin-url').val(curCat.url);
+        $adminInfo.find('#admin-clicks').val(curCat.number);
+
+        $adminInfo.show();
+      } else { // close
+        $adminInfo.hide();
+      }
+
     }
 
 
