c50b446f4174e32105edd84843e302ef21866a15
meng <wulalameng@gmail.com>
2017-06-13 15:01:29 -0400

    two cats

--- js/app.js
+++ js/app.js
@@ -1,11 +1,19 @@
 (function(){
   'use strict';
-  var catImg = document.querySelector('.catImg');
+  var catContainers = document.querySelectorAll('.catContainer');
   var counterScore = document.querySelector('.score');
-  catImg.addEventListener('click', function(e) {
-    var score = parseInt(counterScore.textContent);
+  var catNames = ['cat_1', 'cat_2'];
+
+  var addCounter = function() {
+    var score = parseInt(counterScore.textContent)||0;
     score += 1;
     counterScore.textContent=score;
-  });
+  };
+  for(var i = 0; i < catContainers.length; i++) {
+    var cat = catContainers[i];
+    cat.querySelector('.catName').textContent = catNames[i];
+    // var catImg = cat.querySelector('.catImg');
+    cat.addEventListener('click', addCounter);
+  }
 
 })();
