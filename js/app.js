12ee21dd056a2c778b990c36bb888b78c66770dc
meng <wulalameng@gmail.com>
2017-06-14 20:56:19 -0400

    finish KnockoutJS result

--- js/app.js
+++ js/app.js
@@ -1,6 +1,50 @@
 (function(){
   'use strict';
   // Cat class
+  var initialCats = [
+    {
+      id: 0,
+      name: 'cat_1',
+      src: 'images/cat_1.jpg',
+      clickCount: 0,
+      nickNames: [ 'nn1', 'nn2', 'nn3', 'nn4' ]
+    },
+    {
+      id: 1,
+      name: 'cat_2',
+      src: 'images/cat_2.jpg',
+      clickCount: 0,
+      nickNames: [ 'nna1', 'nna2', 'nna3', 'nna4' ]
+    },
+    {
+      id: 2,
+      name: 'cat_3',
+      src: 'images/cat_3.jpg',
+      clickCount: 0,
+      nickNames: [ 'nnb1', 'nnb2', 'nnb3', 'nnb4' ]
+    },
+    {
+      id: 3,
+      name: 'cat_4',
+      src: 'images/cat_4.jpg',
+      clickCount: 0,
+      nickNames: [ 'nnc1', 'nnc2', 'nnc3', 'nnc4' ]
+    },
+    {
+      id: 4,
+      name: 'cat_5',
+      src: 'images/cat_5.jpg',
+      clickCount: 0,
+      nickNames: [ 'nnd1', 'nnd2', 'nnd3', 'nnd4' ]
+    },
+    {
+      id: 5,
+      name: 'cat_6',
+      src: 'images/cat_6.jpg',
+      clickCount: 0,
+      nickNames: [ 'nne1', 'nne2', 'nne3', 'nne4' ]
+    }
+  ];
   var Cat = function(data) {
     var self = this;
     self.clickCount = ko.observable(data.clickCount);
@@ -34,20 +78,23 @@
 
   var ViewModel = function() {
     var self = this;
-    self.currentCat = ko.observable(new Cat({
-      name: 'cat1',
-      clickCount: 0,
-      src: 'images/cat_1.jpg',
-      nickNames: [
-        'nn1',
-        'nn2',
-        'nn3',
-        'nn4'
-      ]
-    }));
+    // make cat list
+    self.catList = ko.observableArray([]);
+    initialCats.forEach(function(cat){
+      self.catList.push(new Cat(cat));
+    });
+
+    // set the currentCat to the first cat
+    self.currentCat = ko.observable(self.catList()[0]);
+    // increase the clickCount
     self.increaseCount = function() {
       self.currentCat().increaseCount();
     };
+    // change the currentCat
+    self.changeCurrentCat = function(cat) {
+      self.currentCat(cat);
+    };
+
   };
 
   ko.applyBindings(new ViewModel());
